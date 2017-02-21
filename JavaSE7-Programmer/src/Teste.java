ece0c622f4a54034138f9e6dc803f5f7172a40ae
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-20 22:22:44 -0300

    C1M4_1

---
+++ JavaSE7-Programmer/src/Teste.java
@@ -0,0 +1,9 @@
+import modelo.Cliente;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		new Cliente("guilherme")
+	}
+
+}
---
+++ JavaSE7-Programmer/src/modelo/Cliente.java
@@ -0,0 +1,12 @@
+package modelo;
+
+class Cliente {
+	private String nome;
+	Cliente(String nome){
+		this.nome = nome;
+	}
+	
+	public void imprime(){
+		System.out.println(nome);
+	}
+}
