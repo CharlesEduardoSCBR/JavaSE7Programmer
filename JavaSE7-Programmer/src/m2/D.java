7b963621aa24c9f740b8b11aa837cba9fd996c2a
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-20 21:15:25 -0300

    C1M2-2

--- JavaSE7-Programmer/src/m2/D.java
+++ JavaSE7-Programmer/src/m2/D.java
@@ -1,23 +1,14 @@
 package m2;
 
-import java.util.*;
 
 class D {
-    public static void main(String[] args) {
-        ArrayList<String> lista = new ArrayList<String>();
-
-        for (String arg : args) {
-            if (new E().existe(arg))
-                lista.add(arg);
-        }
-    }
-}
-
-import java.io.*;
-
-class E {
-    public boolean existe(String nome) {
-        File f = new File(nome);
-        return f.exists();
-    }
+	int D = 305;
+	
+	void D(){
+		System.out.println(D);
+	}
+	
+	public static void main(String[] args) {
+		new D();
+	}
 }
